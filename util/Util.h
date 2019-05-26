495f2857b8813cb5f2e8c3996a53f9826099c159
vainamoinen <weixinluwx@foxmail.com>
2019-05-26 21:18:45 +0800

    rmat generator

--- util/Util.h
+++ util/Util.h
@@ -1,5 +1,60 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/rmat_graph_generator.hpp>
 #include <boost/random/linear_congruential.hpp>
 #include <boost/graph/graph_traits.hpp>
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <regex.h>
+#include <locale.h>
+#include <assert.h>
+#include <libgen.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+//NOTICE:below are restricted to C++, C files should not include(maybe nested) this header!
+#include <bitset>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+//NOTICE:below are libraries need to link
+#include <math.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
